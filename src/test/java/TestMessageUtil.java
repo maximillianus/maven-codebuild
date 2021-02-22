1efd20a19051eec8ef5a64fa856ff732516f2577
maximillianus <apradana@08f8bc6adc08.ant.amazon.com>
2021-02-22 12:20:44 +0700

    modify test message util

--- src/test/java/TestMessageUtil.java
+++ src/test/java/TestMessageUtil.java
@@ -16,7 +16,7 @@ public class TestMessageUtil {
   @Test
   public void testSalutationMessage() {
     System.out.println("Inside testSalutationMessage()");
-    message = "Hi!" + "Robert";
+    message = "Hi!" + "Robert" + "| Welcome!";
     assertEquals(message,messageUtil.salutationMessage());
   }
 }
